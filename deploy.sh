330198d8952f7e82ec8bcdb0d06f86fa7aedb566
Kevin George <kevingeorgetm@gmail.com>
2016-04-22 12:59:12 -0500

    Update to deployment

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,6 @@ cd `dirname "${BASH_SOURCE[0]}"`
 git pull
 jekyll build --config deploy.conf.yml || _die 1 "Failed to generate website"
 rsync -az --delete --partial --info=progress2 -e "ssh" _site/* \
-    ${UTWEB_USER}@panel.utweb.utexas.edu:~/utw10091/public_html || \
+    ${UTWEB_USER}@panel.utweb.utexas.edu:/home/utweb/utw10091/public_html || \
     _die 2 "Failed to upload website"
+ssh ${UTWEB_USER}@panel.utweb.utexas.edu chown -R ${UTWEB_USER}:utw10091 /home/utweb/utw10091/public_html/*
