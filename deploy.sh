de5201c95d4df5a26d0746131661c629439ff80d
qaute <daniel@teals.org>
2018-02-03 18:34:17 -0600

    Added website deployment script.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Will, assuming you have the correct permissions, deploy the UT RAS site.
+# Define the environment variable UTWEB_USER to be your utweb username.
+
+set -o nounset
+set -o errexit
+
+_die () { err_code=$1; shift 1; echo $@ >&2; exit $err_code; }
+
+jekyll build || _die 1 "Failed to generate website"
+rsync -rlz --delete --partial --info=progress2 -e "ssh" _site/* \
+    ${UTWEB_USER}@panel.utweb.utexas.edu:/home/utweb/utw10091/public_html || \
+    _die 2 "Failed to upload website"
+ssh ${UTWEB_USER}@panel.utweb.utexas.edu chown -R ${UTWEB_USER}:utw10091 /home/utweb/utw10091/public_html/*
